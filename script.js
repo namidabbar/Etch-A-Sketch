2c6c14dc1197d71249f22d73ecb6ae375bdbc452
AnamK <119437655+namidabbar@users.noreply.github.com>
2022-12-22 02:26:35 +0000

    16x16 grid formed using double loops

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const container = document.querySelector('#container');
+let rows;
+let divs;
 
-for (let i=0; i<=16; i++){
-const divs = document.createElement('div');
-divs.classList.add('divs');
-divs.textContent = 'This is div number ' +i;
-container.appendChild(divs);
+
+for (j=0; j<16; j++){
+rows=document.createElement('div');
+rows.classList.add('rows');
+container.appendChild(rows)
+    for (let i=0; i<16; i++){
+    divs = document.createElement('div');
+    divs.classList.add('divs');
+    divs.textContent = `j` +`i`;
+    rows.appendChild(divs);
+    }
 }
